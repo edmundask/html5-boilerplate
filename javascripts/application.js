66deee6cd49e7ebe92bbf24ea74b5d8869fbaf9c
Edmundas Kondrasovas <as@edmundask.lt>
2012-10-28 13:52:06 +0200

    Update file structure for requireJS

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,74 +1,12 @@
-/*!
- * Main application JS code
- * @author Edmundas Kondrašovas
- */
- 
-/*************************************************************************/
-/* Application
-/*************************************************************************/
+requirejs.config({
+    baseUrl: 'javascripts/vendor/lib',
+    paths: {
+        app: '../../app'
+    }
+});
 
-(function(window, undefined){
-
-  // application object
-  var App = {};
-    
-    // stores array of queued ajax requests
-    App.queue = [];
-    
-    // stores array of cached objects, primarily DOM selectors
-    App.cache = {};
-    
-    // stores the url of your application
-    App.url = '';
-    
-    // subscriptions
-    App.subscriptions = {};
-  
-    // modified version of peter higgens pubsub
-    // https://github.com/phiggins42/bloody-jquery-plugins/blob/master/pubsub.js
-    App.publish = function(topic, args){
-      App.subscriptions[topic] && $.each(App.subscriptions[topic], function(){
-          this.apply(App, args || []);
-      });
-    };
-  
-    App.subscribe = function(topic, callback){
-        if(!App.subscriptions[topic]){
-            App.subscriptions[topic] = [];
-        }
-        App.subscriptions[topic].push(callback);
-        return [topic, callback];
-    };
-    
-    App.unsubscribe = function(handle){
-        var t = handle[0];
-        App.subscriptions[t] && $.each(App.subscriptions[topic], function(idx){
-            if(this == handle[1]){
-                App.subscriptions[topic].splice(idx, 1);
-            }
-        });
-    };
-    
-  /*************************************************************************/
-  /* Subscriptions
-  /*************************************************************************/
-  
-  App.subscribe('init', function(){
-     
-  });
-  
-  /*************************************************************************/
-  /* Events
-  /*************************************************************************/
-  
-  // DOM Ready
-  jQuery(function($){
-      App.publish('init');
-  });
-  
-  // Window unload
-  jQuery(window).unload(function(){
-      App.publish('destroy');
-  });
-
-})(window);
+// Start the main app logic.
+requirejs(['jquery', 'app/sub'],
+function ($, sub) {
+    console.log('Everything is loaded.');
+});
